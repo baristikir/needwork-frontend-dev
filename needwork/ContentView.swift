6be9cb967931a6b52423e06ac4f03ff8b9d02ae5
Baris Tikir <brstkr@icloud.com>
2020-07-25 19:34:34 +0200

    Initial Commit

---
+++ needwork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  needwork
+//
+//  Created by Baris Tikir on 25.07.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
