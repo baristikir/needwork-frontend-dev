df63eb54460ba70768f2473de94d5cddf319cf2b
Baris Tikir <brstkr@icloud.com>
2020-08-08 00:42:36 +0200

    Added Fav & Profile View and Deleted unused code

---
+++ needwork/FavoritesView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesView.swift
+//  needwork
+//
+//  Created by Baris Tikir on 08.08.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
---
+++ needwork/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  needwork
+//
+//  Created by Baris Tikir on 08.08.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
